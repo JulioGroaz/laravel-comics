05c4ff652ec210a124b554e000a5914c46715a76
JulioGroaz <julio.groaz2020@gmail.com>
2024-07-15 18:11:32 +0200

    impostato scss

---
+++ laravel-comics/resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel-comics</title>
+
+    @vite('resources/js/app.js')
+</head>
+<body>
+
+    <header>
+        <h1>CIAO</h1>
+    </header>
+
+</body>
+</html>
